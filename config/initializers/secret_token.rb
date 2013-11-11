23b01f9dd6ff5de171d8ca924881bc7220bd99f0
Firehose <firehose@Kens-MacBook-Pro.local>
2013-11-10 19:45:34 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirehoseApp::Application.config.secret_key_base = '0a9cb57eac43001d31c0c08cc26c9725fb1e1f068edead3f50b161d7cef45a047a4c5c85846bf1c5eabfad492dea29208c9feb3a7e4d10f4825240f131439305'
